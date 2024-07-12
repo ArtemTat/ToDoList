b50cf090c105709591bae724ccbfd1ce180f0557
ArtemTat <dondigidon456123@mail.ru>
2024-07-12 20:56:25 +0800

    first

---
+++ ToDoList/Controllers/HomeController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ToDoList.Controllers
+{
+    public class TaskController : Controller
+    {
+        
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        
+    }
+}
